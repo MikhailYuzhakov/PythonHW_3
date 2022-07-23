2e0f981961ddba979c5442ce450e8de09f37d00c
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-07-23 12:19:27 +0300

    произведение пары чисел

--- Task2.py
+++ Task2.py
@@ -2,12 +2,14 @@
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 from random import randint
+
+from numpy import rint
 mltpx = [] # переменная для хранения результата перемножения пар чисел
 
 list = [randint(1, 101) for i in range(1, 11)] # генерируем список из 10 элементов, состоящий из случайных чисел от 1 до 101...
 print(list)
 
 N = len(list)
-for i in range(N):
+for i in range(N//2):
     mltpx.append(list[i] * list[N-1]) # переменожаем пары чисел
 print(mltpx)
